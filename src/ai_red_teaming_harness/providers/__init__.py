26608625d350597c36b889c102ef1fe21fedfddd
Muhammad Sameer <msameerrajput2020@gmail.com>
2026-08-21 12:20:31 +0500

    Update provider package exports

--- src/ai_red_teaming_harness/providers/__init__.py
+++ src/ai_red_teaming_harness/providers/__init__.py
@@ -2,12 +2,16 @@
 
 from .base import ChatProvider
 from .factory import get_provider
+from .mock_flaky import MockFlakyProvider
 from .mock_hardened import MockHardenedProvider
 from .mock_vulnerable import MockVulnerableProvider
+from .openai_live import OpenAILiveProvider
 
 __all__ = [
     "ChatProvider",
+    "MockFlakyProvider",
     "MockHardenedProvider",
     "MockVulnerableProvider",
+    "OpenAILiveProvider",
     "get_provider",
 ]
